e704df92217c55dfd2ccfb74b5be041e71263a40
Loic <loicgevrey@gmail.com>
2014-01-04 12:22:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt-get install nodejs php5-common php5-fpm php5-cli php5-curl php5-json
 useradd jeedom
 mkdir -p /usr/share/nginx/www
 cd /usr/share/nginx/www
-git clone https://github.com/zoic21/jeedom.git
+git clone -b stable --depth 1 https://github.com/zoic21/jeedom.git
 chmod 777 -R log
 cp jeedom /etc/init.d/
 update-rc.d jeedom defaults
