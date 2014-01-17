3fabae3a75f462183883193b07f1ae7787bb4b72
loic <lgevrey@oxya.com>
2014-01-17 14:04:23 +0100

    Ajout consigne dans l'aide

--- desktop/modal/help.scenario.php
+++ desktop/modal/help.scenario.php
@@ -15,6 +15,7 @@ if (!isConnect()) {
         </div>
         <div id="collapse_condition" class="panel-collapse collapse">
             <div class="panel-body">
+                <div class="alert alert-danger">Attention vous ne pouvez comparer 2 chaines de caractere contenant des " " (espace) remplacer les avec des "_" </div>
                 Vous pouvez utiliser n'importe lequel des symboles suivant pour les opérateurs : 
                 <pre>
 = : égal
