2560cc589423ef487502ed5afc0080212fc9ac64
loic <lgevrey@oxya.com>
2014-01-29 14:44:21 +0100

    Update install script

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ apt-get install mysql-client mysql-common mysql-server mysql-server-core
 apt-get install nodejs php5-common php5-fpm php5-cli php5-curl php5-json
 mkdir -p /usr/share/nginx/www
 cd /usr/share/nginx/www
-git clone -b stable --depth 1 https://github.com/zoic21/jeedom.git
+sudo -u www-data -H git clone -b stable --depth 1 https://github.com/zoic21/jeedom.git
 chmod 777 -R log
 cp jeedom /etc/init.d/
 update-rc.d jeedom defaults
