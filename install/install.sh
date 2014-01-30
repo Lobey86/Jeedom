285c5b64bff3cadc9e8df38888340ebbac27f9a5
loic <lgevrey@oxya.com>
2014-01-30 15:40:16 +0100

    Amélioration doc d'installation

--- install/install.sh
+++ install/install.sh
@@ -4,8 +4,8 @@ apt-get install mysql-client mysql-common mysql-server mysql-server-core
 apt-get install nodejs php5-common php5-fpm php5-cli php5-curl php5-json
 mkdir -p /usr/share/nginx/www
 cd /usr/share/nginx/www
+sudo chown www-data:www-data -R /usr/share/nginx/www
 sudo -u www-data -H git clone -b stable --depth 1 https://github.com/zoic21/jeedom.git
-chmod 777 -R log
 cp jeedom /etc/init.d/
 update-rc.d jeedom defaults
 service jeedom start
