80d6b8d03829cce4716ea522b44213b6b4fcf80a
loic <lgevrey@oxya.com>
2014-02-14 17:52:25 +0100

    Ajout de la gestion du market

---
+++ core/class/market.class.php
@@ -0,0 +1,243 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../core/php/core.inc.php';
+
+class market {
+    /*     * *************************Attributs****************************** */
+
+    private $id;
+    private $name;
+    private $type;
+    private $datetime;
+    private $description;
+    private $categorie;
+    private $changelog;
+    private $version;
+    private $user_id;
+    private $path;
+    private $downloaded;
+    private $status;
+    private $author;
+
+    /*     * ***********************Methode static*************************** */
+
+    private static function construct($_arrayMarket) {
+        $market = new market();
+        $market->setId($_arrayMarket['id']);
+        $market->setName($_arrayMarket['name']);
+        $market->setType($_arrayMarket['type']);
+        $market->setDatetime($_arrayMarket['datetime']);
+        $market->setDescription($_arrayMarket['description']);
+        $market->setDownloaded($_arrayMarket['downloaded']);
+        $market->setUser_id($_arrayMarket['user_id']);
+        $market->setVersion($_arrayMarket['version']);
+        $market->setCategorie($_arrayMarket['categorie']);
+        $market->setPath($_arrayMarket['path']);
+        $market->setStatus($_arrayMarket['status']);
+        $market->setAuthor($_arrayMarket['author']);
+        $market->setChangelog($_arrayMarket['changelog']);
+        return $market;
+    }
+
+    public static function byId($_id) {
+        $market = market::getJsonRpc();
+        if ($market->sendRequest('market::byId', array('id' => $_id))) {
+            return self::construct($market->getResult());
+        } else {
+            throw new Exception($market->getError());
+        }
+    }
+
+    public static function byStatusAndType($_status, $_type) {
+        $market = market::getJsonRpc();
+        if ($market->sendRequest('market::byStatusAndType', array('status' => $_status, 'type' => $_type))) {
+            $return = array();
+            foreach ($market->getResult() as $result) {
+                $return[] = self::construct($result);
+            }
+            return $return;
+        } else {
+            throw new Exception($market->getError());
+        }
+    }
+
+    public static function getJsonRpc() {
+        return new jsonrpcClient(config::byKey('market::address') . '/core/api/api.php');
+    }
+
+    /*     * *********************Methode d'instance************************* */
+
+    public function install() {
+        $tmp = dirname(__FILE__) . '/../../tmp/' . $this->getName() . '.zip';
+        $pluginDir = dirname(__FILE__) . '/../../plugins/' . $this->getName();
+        $url = config::byKey('market::address') . "/core/php/downloadFile.php?id=" . $this->getId();
+        file_put_contents($tmp, fopen($url, 'r'));
+        if (!file_exists($tmp)) {
+            throw new Exception('Impossible de télécharger le fichier depuis : ' . $url);
+        }
+        switch ($this->getType()) {
+            case 'plugin' :
+                if (!file_exists($pluginDir) && !mkdir($pluginDir, 0775, true)) {
+                    throw new Exception('Impossible de créer le dossier  : ' . $pluginDir . '. Problème de droits ?');
+                }
+                break;
+        }
+        $zip = new ZipArchive;
+        if ($zip->open($tmp) === TRUE) {
+            $zip->extractTo($pluginDir . '/');
+            $zip->close();
+            switch ($this->getType()) {
+                case 'plugin' :
+                    try {
+                        $plugin = new plugin($this->getName());
+                    } catch (Exception $e) {
+                        $this->remove();
+                        throw new Exception('Impossible d\'installer le plugin. Le nom du plugin est différent de l\'ID ou le plugin n\'est pas correctement formé. Veuillez contacter l\'auteur.');
+                    }
+                    if (config::byKey('installVersionDate', $this->getName()) != '') {
+                        if (is_object($plugin) && $plugin->isActive()) {
+                            $plugin->setIsEnable(1);
+                        }
+                    }
+                    config::save('installVersionDate', $this->getDatetime(), $this->getName());
+                    break;
+            }
+        } else {
+            throw new Exception('Impossible de décompresser le zip : ' . $tmp);
+        }
+    }
+
+    public function remove() {
+        $pluginDir = dirname(__FILE__) . '/../../plugins/' . $this->getName();
+        if (file_exists($pluginDir)) {
+            rrmdir($pluginDir);
+        }
+        config::remove('installVersionDate', $this->getName());
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function getDatetime() {
+        return $this->datetime;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function getCategorie() {
+        return $this->categorie;
+    }
+
+    public function getVersion() {
+        return $this->version;
+    }
+
+    public function getUser_id() {
+        return $this->user_id;
+    }
+
+    public function getPath() {
+        return $this->path;
+    }
+
+    public function getDownloaded() {
+        return $this->downloaded;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setType($type) {
+        $this->type = $type;
+    }
+
+    public function setDatetime($datetime) {
+        $this->datetime = $datetime;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function setCategorie($categorie) {
+        $this->categorie = $categorie;
+    }
+
+    public function setVersion($version) {
+        $this->version = $version;
+    }
+
+    public function setUser_id($user_id) {
+        $this->user_id = $user_id;
+    }
+
+    public function setPath($path) {
+        $this->path = $path;
+    }
+
+    public function setDownloaded($downloaded) {
+        $this->downloaded = $downloaded;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function setAuthor($author) {
+        $this->author = $author;
+    }
+    
+    public function getChangelog() {
+        return $this->changelog;
+    }
+
+    public function setChangelog($changelog) {
+        $this->changelog = $changelog;
+    }
+
+}
+
+?>
---
+++ desktop/modal/market.display.php
@@ -0,0 +1,167 @@
+<?php
+if (!isConnect('admin')) {
+    throw new Exception('401 Unauthorized');
+}
+
+$market = market::byId(init('id'));
+$update = '';
+
+if (config::byKey('installVersionDate', $market->getName()) != '' && config::byKey('installVersionDate', $market->getName()) < $market->getDatetime()) {
+    echo '<div style="width : 100%" class="alert alert-warning" id="div_pluginUpdate">Une mise à jour est disponible. Cliquez sur installer pour l\'effectuer</div>';
+}
+?>
+
+<div style="display: none;width : 100%" id="div_alertMarketDisplay"><?php echo $update; ?></div>
+
+
+<a class="btn btn-success pull-right" href="<?php echo config::byKey('market::address') . "/core/php/downloadFile.php?id=" . $market->getId() ?>" style="color : white;"><i class="fa fa-cloud-download"></i> Télécharger</a>
+<a class="btn btn-warning pull-right" style="color : white;" id="bt_installFromMarket" data-market_id="<?php echo $market->getId(); ?> "><i class="fa fa-plus-circle"></i> Installer</a>
+
+<?php if (config::byKey('installVersionDate', $market->getName()) != '') { ?>
+    <a class="btn btn-danger pull-right" style="color : white;" id="bt_removeFromMarket" data-market_id="<?php echo $market->getId(); ?> "><i class="fa fa-minus-circle"></i> Supprimer</a>
+<?php } ?>
+<br/><br/><br/>
+<form class="form-horizontal" role="form">
+    <div class="row">
+        <div class="col-md-5">
+
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Nom</label>
+                <div class="col-lg-8">
+                    <input class="form-control marketAttr" data-l1key="id" style="display: none;">
+                    <span class="label label-success marketAttr" data-l1key="name" placeholder="Nom"></span>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Type</label>
+                <div class="col-lg-8">
+                    <select class="form-control marketAttr" data-l1key="type" disabled>
+                        <option value="plugin">Plugin</option>
+                        <option value="widget">Widget</option>
+                        <option value="zwave_module">[Zwave] Configuration module</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Auteur</label>
+                <div class="col-lg-8">
+                    <span class="label label-success" ><?php echo $market->getAuthor() ?></span>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Description</label>
+                <div class="col-lg-8">
+                    <span class="label label-primary marketAttr" data-l1key="description" placeholder="Description" style="height: 100px;"></span>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Changelog</label>
+                <div class="col-lg-8">
+                    <span class="label label-default marketAttr" data-l1key="changelog" placeholder="Changelog" style="height: 100px;"></span>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Status</label>
+                <div class="col-lg-8">
+                    <select class="form-control marketAttr" data-l1key="status" disabled>
+                        <option>A valider</option>
+                        <option>Validé</option>
+                        <option>Refusé</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Catégorie</label>
+                <div class="col-lg-8">
+                    <span class="label label-warning marketAttr" data-l1key="categorie" placeholder="Catégorie"></span>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Version</label>
+                <div class="col-lg-8">
+                    <span class="label label-success marketAttr" data-l1key="version" placeholder="Version" ></span>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Derniere modification de l'archive</label>
+                <div class="col-lg-6">
+                    <span class="marketAttr label label-info" data-l1key="datetime"></span>
+                </div>
+            </div>
+            <?php if (config::byKey('installVersionDate', $market->getName()) != '') { ?>
+                <div class="form-group">
+                    <label class="col-lg-4 control-label">Version utilisé actuelement</label>
+                    <div class="col-lg-6">
+                        <span class="marketAttr label label-info" ><?php echo config::byKey('installVersionDate', $market->getName()); ?></span>
+                    </div>
+                </div>
+            <?php } ?>
+            <div class="form-group">
+                <label class="col-lg-4 control-label">Nombre de téléchargement</label>
+                <div class="col-lg-8">
+                    <span class="marketAttr label label-info" data-l1key="downloaded"></span>
+                </div>
+            </div>
+        </div> 
+        <div class="col-md-7">
+            <div class="form-group">
+                <div class="col-lg-12">
+                    <img   src="<?php echo config::byKey('market::address') . '/market/' . $market->getType() . '/' . $market->getName() . '.jpg'; ?>"  class="img-responsive img-thumbnail" />
+                </div>
+            </div>
+        </div> 
+    </div> 
+</form>
+
+<script>
+    $('body').setValues(json_decode('<?php echo json_encode(utils::o2a($market)) ?>'), '.marketAttr');
+
+    $('#bt_installFromMarket').on('click', function() {
+        var id = $(this).attr('data-market_id');
+        $.ajax({// fonction permettant de faire de l'ajax
+            type: "POST", // methode de transmission des données au fichier php
+            url: "core/ajax/market.ajax.php", // url du fichier php
+            data: {
+                action: "install",
+                id: id
+            },
+            dataType: 'json',
+            error: function(request, status, error) {
+                handleAjaxError(request, status, error);
+            },
+            success: function(data) { // si l'appel a bien fonctionné
+                if (data.state != 'ok') {
+                    $('#div_alertMarketDisplay').showAlert({message: data.result, level: 'danger'});
+                    return;
+                }
+                $('#div_pluginUpdate').remove();
+                $('#div_alertMarketDisplay').showAlert({message: 'Plugin installé. Rechargé la page pour mettre à jour', level: 'success'});
+            }
+        });
+    });
+
+    $('#bt_removeFromMarket').on('click', function() {
+        var id = $(this).attr('data-market_id');
+        $.ajax({// fonction permettant de faire de l'ajax
+            type: "POST", // methode de transmission des données au fichier php
+            url: "core/ajax/market.ajax.php", // url du fichier php
+            data: {
+                action: "remove",
+                id: id
+            },
+            dataType: 'json',
+            error: function(request, status, error) {
+                handleAjaxError(request, status, error);
+            },
+            success: function(data) { // si l'appel a bien fonctionné
+                if (data.state != 'ok') {
+                    $('#div_alertMarketDisplay').showAlert({message: data.result, level: 'danger'});
+                    return;
+                }
+                $('#div_alertMarketDisplay').showAlert({message: 'Plugin supprimé. Rechargé la page pour mettre à jour', level: 'success'});
+            }
+        });
+    });
+</script>
