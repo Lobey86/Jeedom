947c1593a03725e2f57b74e13f5658ce19abf01c
loic <lgevrey@oxya.com>
2014-02-16 11:43:43 +0100

    Move some plugin to market

--- plugins/jeenode/desktop/template/jeenode/none.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . '/../../../../../core/php/core.inc.php');
-include_file('core', 'authentification', 'php');
-
-if (!isConnect()) {
-    throw new Exception('401 Unauthorized');
-}
-?>
