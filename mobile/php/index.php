b7d04b44daeaa3bf6ee0efdfdc238eb8733e6e95
Loic <loicgevrey@gmail.com>
2014-03-30 18:39:40 +0200

    Ajout manifest et optimisation

--- mobile/php/index.php
+++ mobile/php/index.php
@@ -25,7 +25,7 @@ if ($plugin != '') {
 $uniquePhpId = time();
 ?>
 <!DOCTYPE html> 
-<html manifest="mobile/php/app.manifest">
+<html manifest="manifest.webapp">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" /> 
