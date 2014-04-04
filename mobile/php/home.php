611d3c1c31c381d85d2b257af7ba5e9e5cff7e24
Loic <loicgevrey@gmail.com>
2014-04-04 15:10:55 +0200

    Typo fix

--- mobile/php/home.php
+++ mobile/php/home.php
@@ -13,7 +13,7 @@ if (!isConnect()) {
             <div data-role="popup" id="equipmentMenu" data-theme="b">
                 <a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Fermer</a>
                 <ul data-role="listview" data-inset="true" style="min-width:210px;">
-                    <li data-role="list-divider">Selectionner la vue</li>
+                    <li data-role="list-divider">Selectionner l'object</li>
                     <?php
                     foreach (object::all() as $object) {
                         echo '<li class="li_object"><a href="index.php?v=m&p=equipment&object_id=' . $object->getId() . '" object_id="' . $object->getId() . '" father_id="' . $object->getFather_id() . '">' . $object->getName() . '</a></li>';
@@ -32,20 +32,14 @@ if (!isConnect()) {
         </center>
     </div>
 
+
+
     <div class="ui-block-a">
         <center>
-            <a href="index.php?v=m&p=message" data-role="button" data-theme="a" style="margin: 5px;">
-                <i class="fa fa-envelope-o" style="font-size: 6em;"></i><br/><?php echo message::nbMessage(); ?> Message(s)
-            </a>
-        </center>
-    </div>
-
-    <div class="ui-block-b">
-        <center>
-            <a href="#vueMenu" data-rel="popup" data-transition="slideup" data-position-to="window" class="ui-btn ui-corner-all ui-shadow ui-btn-a" style="margin: 5px;">
+            <a href="#viewMenu" data-rel="popup" data-transition="slideup" data-position-to="window" class="ui-btn ui-corner-all ui-shadow ui-btn-a" style="margin: 5px;">
                 <i class="fa fa-picture-o" style="font-size: 6em;"></i><br/>Vues
             </a>
-            <div data-role="popup" id="vueMenu" data-theme="b">
+            <div data-role="popup" id="viewMenu" data-theme="b">
                 <a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Fermer</a>
                 <ul data-role="listview" data-inset="true" style="min-width:210px;">
                     <li data-role="list-divider">Selectionner la vue</li>
@@ -58,6 +52,15 @@ if (!isConnect()) {
             </div>
         </center>
     </div>
+
+    <div class="ui-block-b">
+        <center>
+            <a href="index.php?v=m&p=message" data-role="button" data-theme="a" style="margin: 5px;">
+                <i class="fa fa-envelope-o" style="font-size: 6em;"></i><br/><?php echo message::nbMessage(); ?> Message(s)
+            </a>
+        </center>
+    </div>
+
     <?php if (config::byKey('enableChat') == 1 && config::byKey('enableNodeJs') == 1) { ?>
         <div class="ui-block-a">
             <center>
