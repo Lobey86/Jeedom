a301fcde967a7116f92ef2a4bdc1038e02470d95
Loic <loicgevrey@gmail.com>
2014-04-12 16:59:05 +0200

    Nouveau cahce ok pour le manifest

--- site.manifest.php
+++ site.manifest.php
@@ -3,36 +3,59 @@ header('Content-type: ');
 require_once dirname(__FILE__) . "/core/php/core.inc.php";
 ?>
 CACHE MANIFEST
- 
+
 # Version <?php echo getVersion('jeedom'); ?>
 
+
+
 CACHE:
-3rdparty/jquery.include/jquery.include.js
-3rdparty/highstock/highcharts-more.js
-3rdparty/highstock/highstock.js
+/core/php/getJS.php?file=3rdparty/jquery.include/jquery.include.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/highstock/highcharts-more.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/highstock/highstock.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/jquery/jquery.min.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/php.js/php.min.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/jquery.mobile/jquery.mobile.min.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/jquery.value/jquery.value.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/jquery.alert/jquery.alert.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=3rdparty/jquery.loading/jquery.loading.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=mobile/js/utils.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=mobile/js/chat.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=mobile/js/equipement.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=mobile/js/view.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=mobile/js/message.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=mobile/js/cron.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=core/js/chatAdapter.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=core/js/cmd.class.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=core/js/core.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=core/js/eqLogic.class.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=core/js/jeedom.class.js&v=<?php echo getVersion('jeedom'); ?>
+
+/core/php/getJS.php?file=core/js/object.class.js&v=<?php echo getVersion('jeedom'); ?>
+
 3rdparty/jquery.mobile/jquery.mobile.css
 3rdparty/font-awesome/css/font-awesome.min.css
-3rdparty/jquery/jquery.min.js
-3rdparty/php.js/php.min.js
-3rdparty/jquery.mobile/jquery.mobile.min.js
-mobile/css/commun.css
-3rdparty/jquery.value/jquery.value.js
-3rdparty/jquery.alert/jquery.alert.js
-3rdparty/jquery.loading/jquery.loading.css
-3rdparty/jquery.loading/jquery.loading.js
-mobile/js/utils.js
-mobile/js/chat.js
-mobile/js/equipement.js
-mobile/js/view.js
-mobile/js/message.js
-mobile/js/cron.js
-core/js/chatAdapter.js
-core/js/cmd.class.js
-core/js/core.js
-core/js/eqLogic.class.js
-core/js/jeedom.class.js
-core/js/object.class.js
 core/css/core.css
+3rdparty/jquery.loading/jquery.loading.css
+mobile/css/commun.css
 core/img/jeedom_ico.png
 3rdparty/font-awesome/fonts/fontawesome-webfont.woff
 3rdparty/jquery.mobile/images/ajax-loader.gif
