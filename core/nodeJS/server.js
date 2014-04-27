034945f9bdea7f42dd5e6e9720b86643ddfce9c9
loic <lgevrey@oxya.com>
2014-04-27 21:20:58 +0200

    Test of improvement nodeJS

--- core/nodeJS/server.js
+++ core/nodeJS/server.js
@@ -1,19 +1,19 @@
 
 /* This file is part of Jeedom.
-*
-* Jeedom is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Jeedom is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
-*/
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 var io = require('socket.io').listen(8070);
@@ -38,9 +38,11 @@ internalServer.get('/', function(req, res) {
     res.send('OK', 200);
 });
 
-io.configure('development', function(){
-  io.set('transports', ['xhr-polling']);
-  io.set('log level', 0);
+io.configure('development', function() {
+    //io.set('transports', ['xhr-polling']);
+    io.set('log level', 0);
+    io.set('browser client minification', true);  // send minified client
+    io.set('browser client etag', true);
 });
 
 var clients = [];
