3db3ad6fe40e9bfdc9f6869094f6f95fe2bc3881
loic <lgevrey@oxya.com>
2014-04-27 18:13:40 +0200

    UI improvement

--- mobile/modal/login.php
+++ mobile/modal/login.php
@@ -9,10 +9,12 @@
             <input type="text" id="in_login_username" value="" placeholder="{{Nom d'utilisateur}}" data-theme="a" />
             <input type="password" id="in_login_password" value="" placeholder="{{Mot de passe}}" data-theme="a" />
             <label>{{Enregistrer cet appareil}}</label>
-            <select id="cb_storeConnection" data-role="slider" data-mini="true">
-                <option value="0">{{Non}}</option>
-                <option value="1">{{Oui}}</option>
-            </select>
+            <center>
+                <select id="cb_storeConnection" data-role="slider" data-mini="true">
+                    <option value="0">{{Non}}</option>
+                    <option value="1">{{Oui}}</option>
+                </select>
+            </center>
 
 
             <a id="bt_login_validate" href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check">{{Connexion}}</a>
