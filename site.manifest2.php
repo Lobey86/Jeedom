9a1e061417ebb4cf7f9dd99afd119740e7f03ae6
loic <lgevrey@oxya.com>
2014-04-30 20:28:57 +0200

    Amélioration systeme de cache

--- site.manifest2.php
+++ site.manifest2.php
@@ -2,11 +2,7 @@
 header('Content-type: ');
 require_once dirname(__FILE__) . "/core/php/core.inc.php";
 ?>
-
-
 CACHE MANIFEST
-
-
 
 # Version <?php echo getVersion('jeedom'); ?>
 
@@ -20,10 +16,6 @@ core/php/getJS.php?file=3rdparty/jquery.mobile/jquery.mobile.min.js
 core/php/getJS.php?file=3rdparty/jquery.value/jquery.value.js
 core/php/getJS.php?file=3rdparty/jquery.alert/jquery.alert.js
 core/php/getJS.php?file=3rdparty/jquery.loading/jquery.loading.js
-core/php/getJS.php?file=mobile/js/equipement.js
-core/php/getJS.php?file=mobile/js/view.js
-core/php/getJS.php?file=mobile/js/message.js
-core/php/getJS.php?file=mobile/js/cron.js
 core/php/getJS.php?file=core/js/cmd.class.js
 core/php/getJS.php?file=core/js/core.js
 core/php/getJS.php?file=core/js/eqLogic.class.js
@@ -33,14 +25,12 @@ core/php/getJS.php?file=core/js/plugin.class.js
 core/php/getJS.php?file=core/js/view.class.js
 core/php/getJS.php?file=core/js/message.class.js
 core/php/getJS.php?file=core/js/scenario.class.js
-core/php/getJS.php?file=mobile/js/application.js
-core/php/getJS.php?file=mobile/js/equipment.js
 core/php/getJS.php?file=3rdparty/jquery.masonry/jquery.masonry.js
 core/php/getJS.php?file=3rdparty/jquery.farbtastic/farbtastic.js
-core/php/getJS.php?file=mobile/css/farbtastic.css
-core/php/getJS.php?file=mobile/css/market.png
-core/php/getJS.php?file=mobile/css/mask.png
-core/php/getJS.php?file=mobile/css/wheel.png
+=mobile/css/farbtastic.css
+mobile/css/market.png
+mobile/css/mask.png
+mobile/css/wheel.png
 
 3rdparty/jquery.mobile/jquery.mobile.css
 3rdparty/font-awesome/css/font-awesome.min.css
@@ -50,14 +40,25 @@ mobile/css/commun.css
 3rdparty/font-awesome/fonts/fontawesome-webfont.woff?v=4.0.3
 3rdparty/jquery.mobile/images/ajax-loader.gif
 
-index.php?v=m&p=view
-index.php?v=m&p=equipment
-index.php?v=m&p=home
-index.php?v=m
-index.php?v=m&p=message
-index.php?v=m&p=scenario
-index.php?v=m&p=cron
-index.php?v=m&modal=login
+<?php
+foreach (ls('mobile/js', '*.js') as $file) {
+    echo 'core/php/getJS.php?file=mobile/js/' . $file . "\n";
+}
+foreach (ls('mobile/html', '*.html') as $file) {
+    echo 'index.php?v=m&p=' . substr($file, 0, -5) . "\n";
+}
+
+foreach (plugin::listPlugin(true) as $plugin) {
+    if ($plugin->getMobile() != '') {
+        foreach (ls('plugins/' . $plugin->getId() . '/mobile/js', '*.js') as $file) {
+            echo 'core/php/getJS.php?file=plugins/' . $plugin->getId() . '/mobile/js/' . $file . "\n";
+        }
+        foreach (ls('plugins/' . $plugin->getId() . '/mobile/html', '*.html') as $file) {
+            echo 'index.php?v=m&m=' . $plugin->getId() . '&p=' . substr($file, 0, -5) . "\n";
+        }
+    }
+}
+?>
 
 NETWORK:
 *
