efdfa94001d6cd368023580ed520a73b57b5eefd
loic <lgevrey@oxya.com>
2014-05-04 20:59:06 +0200

    Fix return fonction get dir size

--- core/php/utils.inc.php
+++ core/php/utils.inc.php
@@ -569,3 +569,28 @@ function calculPath($_path) {
     }
     return $_path;
 }
+
+function getDirectorySize($path) {
+    $totalsize = 0;
+    $totalcount = 0;
+    $dircount = 0;
+    if ($handle = opendir($path)) {
+        while (false !== ($file = readdir($handle))) {
+            $nextpath = $path . '/' . $file;
+            if ($file != '.' && $file != '..' && !is_link($nextpath)) {
+                if (is_dir($nextpath)) {
+                    $dircount++;
+                    $result = getDirectorySize($nextpath);
+                    $totalsize += $result['size'];
+                    $totalcount += $result['count'];
+                    $dircount += $result['dircount'];
+                } elseif (is_file($nextpath)) {
+                    $totalsize += filesize($nextpath);
+                    $totalcount++;
+                }
+            }
+        }
+    }
+    closedir($handle);
+    return $totalsize;
+}
