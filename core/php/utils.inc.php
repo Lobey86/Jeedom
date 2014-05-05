871cb16dc3a8139daa3d9138b605bad09e95ba0f
loic <lgevrey@oxya.com>
2014-05-05 14:29:48 +0200

    Ajout fonciton taille human readable

--- core/php/utils.inc.php
+++ core/php/utils.inc.php
@@ -594,3 +594,13 @@ function getDirectorySize($path) {
     closedir($handle);
     return $totalsize;
 }
+
+function sizeFormat($size) {
+    $mod = 1024;
+    $units = explode(' ', 'B KB MB GB TB PB');
+    for ($i = 0; $size > $mod; $i++) {
+        $size /= $mod;
+    }
+
+    return round($size, 2) . ' ' . $units[$i];
+}
