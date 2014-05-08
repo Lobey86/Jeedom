69aef05ce04d653ad73bf5c4b3d8faba845fa2f6
Loic <loicgevrey@gmail.com>
2014-05-08 14:59:40 +0200

    UI improvement

--- desktop/php/object.php
+++ desktop/php/object.php
@@ -50,7 +50,7 @@ sendVarToJS('select_id', init('id', '-1'));
                 <div class="form-group">
                     <label class="col-sm-1 control-label">{{Visible}}</label>
                     <div class="col-sm-1">
-                        <input class="form-control objectAttr" type="checkbox"  data-l1key="is_visible" checked/>
+                        <input class="objectAttr" type="checkbox"  data-l1key="is_visible" checked/>
                     </div>
                 </div>
             </fieldset>
