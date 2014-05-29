05eb62ec8b565a6388beeb0f12f74fdd3297863a
Loic <loicgevrey@gmail.com>
2014-05-29 11:14:51 +0200

    Ajout atCaret

--- desktop/modal/about.php
+++ desktop/modal/about.php
@@ -53,10 +53,8 @@ if (!isConnect()) {
             <li><a class="cursor" href="http://www.jstree.com/" style="color:blue;" target="_blank">jquery.tree</a></li>
             <li><a class="cursor" href="http://jqueryui.com/" style="color:blue;" target="_blank">jquery.ui</a></li>
             <li>jquery.value - Loïc Gevrey</li>
-            <li><a class="cursor" href="http://justgage.com/" style="color:blue;" target="_blank">justGage</a></li>
             <li><a class="cursor" href="http://phpjs.org/" style="color:blue;" target="_blank">php.js</a></li>
             <li><a class="cursor" href="https://github.com/PHPMailer/PHPMailer" style="color:blue;" target="_blank">PHPMailer</a></li>
-            <li><a class="cursor" href="https://github.com/kbjr/Git.php" style="color:blue;" target="_blank">Git.php</a></li>
         </lu>
     </div>
 </div>
