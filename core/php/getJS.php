da1588b20107ad06f2d1b48115d412437642bd92
Loic <loicgevrey@gmail.com>
2014-05-30 11:42:40 +0200

    Fix nodeJS minify and 304 return

--- core/php/getJS.php
+++ core/php/getJS.php
@@ -26,8 +26,6 @@ if (file_exists($file)) {
     if (extension_loaded("zlib") && (ini_get("output_handler") != "ob_gzhandler")) {
         ini_set("zlib.output_compression", 1);
     }
-
-
     header('Content-Type: application/javascript');
     $lastModified = filemtime($file);
     $etagFile = md5_file($file);
