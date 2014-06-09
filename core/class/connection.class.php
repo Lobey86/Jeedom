8f07f8c5a0ed075ffa1c3c2c50515aa9bf46e06a
Loic <loicgevrey@gmail.com>
2014-06-09 11:53:02 +0200

    Typo fix

--- core/class/connection.class.php
+++ core/class/connection.class.php
@@ -95,7 +95,7 @@ class connection {
         }
         $connection->setFailure(0);
         $connection->setUsername($_username);
-        $connection->setStatus('Connected');
+        $connection->setStatus('Ok');
         $connection->save();
     }
 
