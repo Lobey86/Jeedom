f41d99b047bb0b4f2f19416578748660c800749e
loic <lgevrey@oxya.com>
2014-06-16 11:21:14 +0200

    Improve color

--- core/config/jeedom.config.php
+++ core/config/jeedom.config.php
@@ -21,8 +21,8 @@ $JEEDOM_INTERNAL_CONFIG = array(
     'eqLogic' => array(
         'category' => array(
             'heating' => array('name' => 'Chauffage', 'color' => '#2980b9', 'mcolor' => '#2980b9','cmdColor' => '#3498db','mcmdColor' => '#3498db'),
-            'security' => array('name' => 'Sécurité', 'color' => '#8e44ad', 'mcolor' => '#8e44ad','cmdColor' => '#9b59b6','mcmdColor' => '#9b59b6'),
-            'energy' => array('name' => 'Energie', 'color' => '#27ae60', 'mcolor' => '#27ae60','cmdColor' => '#2ecc71','mcmdColor' => '#2ecc71'),
+            'security' => array('name' => 'Sécurité', 'color' => '#745cb0', 'mcolor' => '#745cb0','cmdColor' => '#ac92ed','mcmdColor' => '#ac92ed'),
+            'energy' => array('name' => 'Energie', 'color' => '#2eb04b', 'mcolor' => '#2eb04b','cmdColor' => '#69e283','mcmdColor' => '#69e283'),
             'light' => array('name' => 'Lumière', 'color' => '#f39c12', 'mcolor' => '#f39c12','cmdColor' => '#f1c40f','mcmdColor' => '#f1c40f'),
             'automatism' => array('name' => 'Automatisme', 'color' => '#808080', 'mcolor' => '#808080','cmdColor' => '#c2beb8','mcmdColor' => '#c2beb8'),
         ),
