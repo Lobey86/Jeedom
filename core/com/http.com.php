c7d609a88d7e556a18f89dfa2d33bf63d522c110
loic <lgevrey@oxya.com>
2014-07-01 11:38:03 +0200

    Fix ssl issue

--- core/com/http.com.php
+++ core/com/http.com.php
@@ -52,6 +52,7 @@ class com_http {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $this->url);
             curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: close'));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_TIMEOUT, $_timeout);
