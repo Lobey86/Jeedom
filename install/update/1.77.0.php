e756bf0b97d49b3cd84378afed0ce178f09555ec
loic <lgevrey@oxya.com>
2014-07-04 17:43:36 +0200

    Fix update

--- install/update/1.77.0.php
+++ install/update/1.77.0.php
@@ -1,4 +1,26 @@
 <?php
+function cast($sourceObject, $destination) {
+    if (is_string($destination)) {
+        $destination = new $destination();
+    }
+    $sourceReflection = new ReflectionObject($sourceObject);
+    $destinationReflection = new ReflectionObject($destination);
+    $sourceProperties = $sourceReflection->getProperties();
+    foreach ($sourceProperties as $sourceProperty) {
+        $sourceProperty->setAccessible(true);
+        $name = $sourceProperty->getName();
+        $value = $sourceProperty->getValue($sourceObject);
+        if ($destinationReflection->hasProperty($name)) {
+            $propDest = $destinationReflection->getProperty($name);
+            $propDest->setAccessible(true);
+            $propDest->setValue($destination, $value);
+        } else {
+            $destination->$name = $value;
+        }
+    }
+    return $destination;
+}
+
 
 foreach (cmd::all() as $cmd) {
     $cmd->setEqType($cmd->getEqLogic()->getEqType_name());
