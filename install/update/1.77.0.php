abb973ce8bab34bc56c20615f9664083cfe8ebb1
Loic <loicgevrey@gmail.com>
2014-07-05 13:14:56 +0200

    Improvement update

--- install/update/1.77.0.php
+++ install/update/1.77.0.php
@@ -1,25 +1,5 @@
 <?php
-function cast($sourceObject, $destination) {
-    if (is_string($destination)) {
-        $destination = new $destination();
-    }
-    $sourceReflection = new ReflectionObject($sourceObject);
-    $destinationReflection = new ReflectionObject($destination);
-    $sourceProperties = $sourceReflection->getProperties();
-    foreach ($sourceProperties as $sourceProperty) {
-        $sourceProperty->setAccessible(true);
-        $name = $sourceProperty->getName();
-        $value = $sourceProperty->getValue($sourceObject);
-        if ($destinationReflection->hasProperty($name)) {
-            $propDest = $destinationReflection->getProperty($name);
-            $propDest->setAccessible(true);
-            $propDest->setValue($destination, $value);
-        } else {
-            $destination->$name = $value;
-        }
-    }
-    return $destination;
-}
+@include dirname(__FILE__) . '/../core/php/utils.inc.php';
 
 
 foreach (cmd::all() as $cmd) {
