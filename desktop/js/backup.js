26fd26289c867cb2b58870c301afe8dcfa1f8eb8
Loic <loicgevrey@gmail.com>
2014-07-05 13:35:20 +0200

    Fix log error on restore

--- desktop/js/backup.js
+++ desktop/js/backup.js
@@ -158,7 +158,9 @@ function getJeedomLog(_autoUpdate, _log) {
         dataType: 'json',
         global: false,
         error: function(request, status, error) {
-            handleAjaxError(request, status, error);
+            setTimeout(function() {
+                getJeedomLog(_autoUpdate, _log)
+            }, 1000);
         },
         success: function(data) {
             if (data.state != 'ok') {
