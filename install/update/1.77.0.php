e8e7b375a69fdddf24053956a1067c132a2c3e85
Loic <loicgevrey@gmail.com>
2014-07-05 13:22:34 +0200

    Fix update

--- install/update/1.77.0.php
+++ install/update/1.77.0.php
@@ -1,5 +1,28 @@
 <?php
-@include dirname(__FILE__) . '/../core/php/utils.inc.php';
+
+if (!function_exists('cast')) {
+    function cast($sourceObject, $destination) {
+        if (is_string($destination)) {
+            $destination = new $destination();
+        }
+        $sourceReflection = new ReflectionObject($sourceObject);
+        $destinationReflection = new ReflectionObject($destination);
+        $sourceProperties = $sourceReflection->getProperties();
+        foreach ($sourceProperties as $sourceProperty) {
+            $sourceProperty->setAccessible(true);
+            $name = $sourceProperty->getName();
+            $value = $sourceProperty->getValue($sourceObject);
+            if ($destinationReflection->hasProperty($name)) {
+                $propDest = $destinationReflection->getProperty($name);
+                $propDest->setAccessible(true);
+                $propDest->setValue($destination, $value);
+            } else {
+                $destination->$name = $value;
+            }
+        }
+        return $destination;
+    }
+}
 
 
 foreach (cmd::all() as $cmd) {
