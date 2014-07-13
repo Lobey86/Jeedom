81a781e5b662487c318355127ac80b93699bed3d
Loic <loicgevrey@gmail.com>
2014-07-13 12:11:27 +0200

    Update doc

--- desktop/modal/help.scenario.php
+++ desktop/modal/help.scenario.php
@@ -83,7 +83,8 @@ if (!isConnect()) {
 {{sleep : pause de x seconde(s)
 variable : création/modification d'une ou de la valeur d'une variable
 scenario : permet le controle des scénarios
-stop : arrête le scénario}}
+stop : arrête le scénario
+icon : permet de changer l'icone de représentation du scenario}}
                 </pre>
                 {{Vous pouvez aussi utiliser les tags suivants dans les options : }}
                 <pre>
