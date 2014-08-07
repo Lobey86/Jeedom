d3079aa1b3bd6d7db0ee96721413d7d0a835e9f7
Loic <loicgevrey@gmail.com>
2014-08-08 00:29:35 +0200

    Code improvement

--- core/class/translate.class.php
+++ core/class/translate.class.php
@@ -35,7 +35,7 @@ class translate {
         }
         $language = self::getLanguage();
         if ($language == 'fr_FR') {
-            return str_replace(array('{{', '}}'), '', $_content);
+            return preg_replace("/{{(.*?)}}/s", '$1', $_content);
         }
         if (substr($_name, 0, 1) == '/') {
             if (strpos($_name, 'plugins') !== false) {
