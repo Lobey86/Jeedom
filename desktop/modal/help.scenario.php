87e49bc441b179a5a4151417ba989136b6891eef
Loic <loicgevrey@gmail.com>
2014-08-09 10:19:53 +0200

    Typo fix

--- desktop/modal/help.scenario.php
+++ desktop/modal/help.scenario.php
@@ -62,7 +62,7 @@ if (!isConnect()) {
 {{rand(1,10) : pour un nombre aléatoire de 1 à 10}}
 {{tendance(commande,period,seuil) : donne la tendance de la commande sur la period 
     Ex : tendance(#[Salle de bain][Hydrometrie][Humidité]#,1 hour,0.1) : Renvoi 1 si en augmentation, 0 si constant et -1 si en diminution
-         Le seuil permet de definir la sensibilitéen, attention le calcul du seuil utilise la calcul de <a target="_blank" href="http://fr.wikipedia.org/wiki/M%C3%A9thode_des_moindres_carr%C3%A9s">moindre carrée</a>}}
+         Le seuil permet de definir la sensibilitéen, attention le calcul du seuil utilise la calcul de <a target="_blank" href="http://fr.wikipedia.org/wiki/M%C3%A9thode_des_moindres_carr%C3%A9s">moindre carrés</a>}}
 {{variable(mavariable,valeur par default) : récupération de la valeur d'une variable ou de la valeur souhaitée par défaut
     Ex : variable(plop,10) renvoie la valeur de la variable plop ou 10 si elle est vide ou n'existe pas}}
 {{scenario(scenario) : donne le statut du scenario
