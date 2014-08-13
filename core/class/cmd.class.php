a2e9219e0947ed7453e33a8a5f459f128dd223a9
loic <lgevrey@oxya.com>
2014-08-13 19:44:34 +0200

    Priorité au scénario

--- core/class/cmd.class.php
+++ core/class/cmd.class.php
@@ -730,7 +730,7 @@ class cmd {
         if ($this->getType() != 'info') {
             return;
         }
-        $collectDate = ($this->getCollectDate() != '' ) ? strtotime($this->getCollectDate()) : strtotime('now');
+        $collectDate = ($this->getCollectDate() != '' ) ? strtotime($this->getCollectDate()) : '';
         if ($this->getCollectDate() != '' && ((strtotime('now') - $collectDate) > 3600 || (strtotime('now') + 300 ) < $collectDate)) {
             return;
         }
@@ -746,6 +746,8 @@ class cmd {
             }
             cache::set('cmd' . $this->getId(), $_value, $this->getCacheLifetime(), array('collectDate' => $this->getCollectDate()));
             $this->setCollect(0);
+            scenario::check($this->getId());
+            
             nodejs::pushUpdate('eventCmd', array('cmd_id' => $this->getId(), 'eqLogic_id' => $this->getEqLogic_id(), 'object_id' => $this->getEqLogic()->getObject_id()));
             foreach (self::byValue($this->getId()) as $cmd) {
                 if ($cmd->getId() != $this->getId()) {
@@ -762,7 +764,7 @@ class cmd {
             $internalEvent->setOptions('value', $_value);
             $internalEvent->setDatetime($this->getCollectDate());
             $internalEvent->save();
-            scenario::check($this->getId());
+            
             listener::check($this->getId(), $_value);
         } else {
             log::add('core', 'Error', __('Impossible de trouver l\'équipement correspondant à l\'id', __FILE__) . $this->getEqLogic_id() . __(' ou équipement désactivé. Evènement sur commande :', __FILE__) . $this->getHumanName(), 'notFound' . $this->getEqLogic_id());
