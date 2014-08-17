79665c3ed68c0e0e14771016c305b6cf140ffa9d
loic <lgevrey@oxya.com>
2014-08-17 22:35:08 +0200

    Try bugfix

--- core/class/scenarioExpression.class.php
+++ core/class/scenarioExpression.class.php
@@ -262,7 +262,7 @@ class scenarioExpression {
                         }
                         if (is_numeric($options['duration']) && $options['duration'] > 0) {
                             $this->setLog(__('Pause de ', __FILE__) . $options['duration'] . __(' seconde(s)', __FILE__));
-                            return sleep(intval($options['duration']));
+                            return sleep($options['duration']);
                         }
                     }
                     $this->setLog(__('Aucune durée trouvée pour l\'action sleep ou la durée n\'est pas valide : ', __FILE__) . $options['duration']);
