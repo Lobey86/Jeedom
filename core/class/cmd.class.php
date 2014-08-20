6c164c4d3d7c33f524498d0329befba891e94da6
loic <lgevrey@oxya.com>
2014-08-20 09:37:28 +0200

    Priorize scenario

--- core/class/cmd.class.php
+++ core/class/cmd.class.php
@@ -771,6 +771,8 @@ class cmd {
             }
             cache::set('cmd' . $this->getId(), $_value, $this->getCacheLifetime(), array('collectDate' => $this->getCollectDate()));
             $this->setCollect(0);
+            scenario::check($this->getId());
+            
             nodejs::pushUpdate('eventCmd', array('cmd_id' => $this->getId(), 'eqLogic_id' => $this->getEqLogic_id(), 'object_id' => $this->getEqLogic()->getObject_id()));
             foreach (self::byValue($this->getId()) as $cmd) {
                 if ($cmd->getId() != $this->getId()) {
@@ -789,7 +791,7 @@ class cmd {
             $internalEvent->setOptions('value', $_value);
             $internalEvent->setDatetime($this->getCollectDate());
             $internalEvent->save();
-            scenario::check($this->getId());
+            
             listener::check($this->getId(), $_value);
         } else {
             log::add('core', 'Error', __('Impossible de trouver l\'équipement correspondant à l\'id', __FILE__) . $this->getEqLogic_id() . __(' ou équipement désactivé. Evènement sur commande :', __FILE__) . $this->getHumanName(), 'notFound' . $this->getEqLogic_id());
