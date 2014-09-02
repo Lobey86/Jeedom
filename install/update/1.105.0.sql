60ee994ff04c16164c444ff77298e3db21d63b50
loic <lgevrey@oxya.com>
2014-09-02 11:50:31 +0200

    Bugfix

--- install/update/1.105.0.sql
+++ install/update/1.105.0.sql
@@ -1,9 +1,6 @@
 ALTER TABLE `jeedom`.`cron` 
 ADD COLUMN `priority` INT(11) NULL DEFAULT NULL AFTER `id`;
 
-ALTER TABLE `jeedom`.`cron` 
-ADD INDEX `deamon` (`deamon` ASC);
-
 ALTER TABLE `jeedom`.`cache` 
 ADD INDEX `lifetime` (`lifetime` ASC);
 
