ce38d4a868835418db3bd8bc5d69d13c1afb5f74
Loic <loicgevrey@gmail.com>
2014-10-02 22:13:51 +0200

    Bugfix market send

--- core/class/jsonrpcClient.class.php
+++ core/class/jsonrpcClient.class.php
@@ -91,10 +91,6 @@ class jsonrpcClient {
         }
         if ($_file !== null) {
             $_request = array_merge($_request, $_file);
-            
-          /*  foreach ($_file as $key => $value) {
-                $_request[$key] = curl_file_create($value);
-            }*/
         }
         $nbRetry = 0;
         while ($nbRetry < $_maxRetry) {
