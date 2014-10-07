46bfc214dfed80b976e55d6011bf012adf64c71b
loic <lgevrey@oxya.com>
2014-10-07 19:54:34 +0200

    Code improvement

--- core/class/jsonrpcClient.class.php
+++ core/class/jsonrpcClient.class.php
@@ -37,7 +37,7 @@ class jsonrpcClient {
                 'method' => $_method,
                 'params' => $_params,
         )));
-        $this->rawResult = trim($this->send($request, $_timeout, $_file, $_maxRetry));
+        $this->rawResult = preg_replace('/[^[:print:]]/', '',trim($this->send($request, $_timeout, $_file, $_maxRetry)));
 
         if ($this->rawResult === false) {
             return false;
