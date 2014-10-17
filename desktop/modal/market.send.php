9b0c0db73813cdd6542242c20ecf7981699edb89
loic <lgevrey@oxya.com>
2014-10-17 16:29:03 +0200

    Typo fix

--- desktop/modal/market.send.php
+++ desktop/modal/market.send.php
@@ -61,7 +61,7 @@ if (init('type') == 'plugin') {
         <div class="col-lg-3">
             <div style="height: 130px;" class="priceChoose">
                 <center><input type="radio" name="rb_price" class="rb_price" data-value="custom" /> <h4 style="display: inline-block">Libre</h4> <input class="form-control marketAttr input-sm" data-l1key="cost" placeholder="Prix" style="display : inline-block; width : 80px;"> €</center>
-                <center>Sur ce prix seront prélevés 0,25€ de frais paypal puis 25% destinés à l'équipe du projet Jeedom (le prix doit etre > 2€)</center>
+                <center>Sur ce prix seront prélevés 0,25€ de frais paypal puis 25% destinés à l'équipe du projet Jeedom (le prix doit etre >= 3€)</center>
             </div>
         </div>
     </div>
