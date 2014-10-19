f98951b7b8da5dcbdd44741548615ff545243c24
Loic <loicgevrey@gmail.com>
2014-10-19 17:35:16 +0200

    Fix bug on plan

--- desktop/js/plan.js
+++ desktop/js/plan.js
@@ -535,7 +535,7 @@ function displayObject(_type, _id, _html, _plan) {
         if (isset(_plan.display) && isset(_plan.display.cmd)) {
             for (var id in _plan.display.cmd) {
                 if (_plan.display.cmd[id] == 1) {
-                    $('.cmd[data-cmd_id=' + id + ']').hide();
+                    $('.cmd[data-cmd_id=' + id + ']').remove();
                 }
             }
         }
