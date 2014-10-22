2646514c9dac7dfd9de00b047443d2536b2b1a09
loic <lgevrey@oxya.com>
2014-10-22 08:23:58 +0200

    Speed improvement

--- desktop/php/index.php
+++ desktop/php/index.php
@@ -268,9 +268,6 @@ if (count($plugins_list) > 0) {
                     if (config::byKey('enableScenario') == 0) {
                         echo '<div style="width : 100%" class="alert alert-warning">{{Erreur scéenario : tous les scénarios sont désactivés aller sur la page des scénarios pour les réactiver}}</div>';
                     }
-                    if (!jeedom::isDateOk()) {
-                        echo '<div style="width : 100%" class="alert alert-warning">{{Erreur date : la date du système est incoherente avec la dernière date enregistrée}}</div>';
-                    }
                     $hw_restrict = jeedom::isRestrictionOk();
                     if (is_numeric($hw_restrict) && $hw_restrict > 1) {
                         echo '<div style="width : 100%" class="alert alert-warning">{{Attention vous utilisez Jeedom sur un hardware soumis à une licence, veuillez enregistrer votre compte market et/ou acheter une licence, il vous reste ' . convertDuration($hw_restrict) . '}}</div>';
