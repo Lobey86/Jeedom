3e8cbaf429b65e7618360ef9faa043270b50827b
loic <lgevrey@oxya.com>
2014-10-29 12:02:51 +0100

    Bugfix history modal

--- desktop/modal/cmd.history.php
+++ desktop/modal/cmd.history.php
@@ -10,22 +10,31 @@ if (!isConnect()) {
         <option value="area"> {{Aire}} </option>
         <option value="column">{{Colonne}}</option>
     </select>
-    <span class="pull-right">Variation : <input type="checkbox" data-cmd_id="#id#" class="cb_derive" /></span>
-    <span class="pull-right">Variation : <input type="checkbox" data-cmd_id="#id#" class="cb_step" /></span>
 
-
+    <?php
+    if (init('derive', 0) == 1) {
+        echo '<span class="pull-right">Variation : <input type="checkbox" data-cmd_id="#id#" class="cb_derive" checked /></span>';
+    } else {
+        echo '<span class="pull-right">Variation : <input type="checkbox" data-cmd_id="#id#" class="cb_derive" /></span>';
+    }
+     if (init('step', 0) == 1) {
+        echo '<span class="pull-right">Escalier : <input type="checkbox" data-cmd_id="#id#" class="cb_step" checked /></span>';
+    } else {
+        echo '<span class="pull-right">Escalier : <input type="checkbox" data-cmd_id="#id#" class="cb_step" /></span>';
+    }
+    ?>
     <script>
-        $('.sel_chartType[data-cmd_id=#id#]').on('change', function() {
+        $('.sel_chartType[data-cmd_id=#id#]').on('change', function () {
             $('#md_modal').dialog({title: "{{Historique}}"});
-            $("#md_modal").load('index.php?v=d&modal=cmd.history&id=<?php echo init('id') ?>&graphType=' + $('.sel_chartType[data-cmd_id=#id#]').value() + '&derive=' + $('.cb_derive[data-cmd_id=#id#]').value()+ '&step=' + $('.cb_step[data-cmd_id=#id#]').value()).dialog('open');
+            $("#md_modal").load('index.php?v=d&modal=cmd.history&id=<?php echo init('id') ?>&graphType=' + $('.sel_chartType[data-cmd_id=#id#]').value() + '&derive=' + $('.cb_derive[data-cmd_id=#id#]').value() + '&step=' + $('.cb_step[data-cmd_id=#id#]').value()).dialog('open');
         });
-        $('.cb_derive[data-cmd_id=#id#]').on('change', function() {
+        $('.cb_derive[data-cmd_id=#id#]').on('change', function () {
             $('#md_modal').dialog({title: "{{Historique}}"});
-            $("#md_modal").load('index.php?v=d&modal=cmd.history&id=<?php echo init('id') ?>&graphType=' + $('.sel_chartType[data-cmd_id=#id#]').value() + '&derive=' + $('.cb_derive[data-cmd_id=#id#]').value()+ '&step=' + $('.cb_step[data-cmd_id=#id#]').value()).dialog('open');
+            $("#md_modal").load('index.php?v=d&modal=cmd.history&id=<?php echo init('id') ?>&graphType=' + $('.sel_chartType[data-cmd_id=#id#]').value() + '&derive=' + $('.cb_derive[data-cmd_id=#id#]').value() + '&step=' + $('.cb_step[data-cmd_id=#id#]').value()).dialog('open');
         });
-         $('.cb_step[data-cmd_id=#id#]').on('change', function() {
+        $('.cb_step[data-cmd_id=#id#]').on('change', function () {
             $('#md_modal').dialog({title: "{{Historique}}"});
-            $("#md_modal").load('index.php?v=d&modal=cmd.history&id=<?php echo init('id') ?>&graphType=' + $('.sel_chartType[data-cmd_id=#id#]').value() + '&derive=' + $('.cb_derive[data-cmd_id=#id#]').value()+ '&step=' + $('.cb_step[data-cmd_id=#id#]').value()).dialog('open');
+            $("#md_modal").load('index.php?v=d&modal=cmd.history&id=<?php echo init('id') ?>&graphType=' + $('.sel_chartType[data-cmd_id=#id#]').value() + '&derive=' + $('.cb_derive[data-cmd_id=#id#]').value() + '&step=' + $('.cb_step[data-cmd_id=#id#]').value()).dialog('open');
         });
         $('#div_historyChart').css('position', 'relative').css('width', '100%');
         delete jeedom.history.chart['div_historyChart'];
@@ -35,8 +44,8 @@ if (!isConnect()) {
             daterange: 'all',
             option: {
                 graphType: "<?php echo init('graphType', 'line') ?>",
-                derive: "<?php echo init('derive', '0') ?>",
-                graphStep :"<?php echo init('step', '0') ?>",
+                derive: "<?php echo init('derive', 0) ?>",
+                graphStep: "<?php echo init('step', 0) ?>",
             }
         });
     </script>
